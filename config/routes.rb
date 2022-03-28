89d3b31d3e41524ef884a96b5298382314959eb7
EC2 Default User <ec2-user@ip-172-31-92-97.ec2.internal>
2022-03-28 14:21:50 +0000

    [Add]ログインページ

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  namespace :admin do
+   resources :genres, only: [:index, :edit]
+  end
+  devise_for :admin
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
