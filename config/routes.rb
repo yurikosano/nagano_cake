810123685bc2ca7866b399402e3f192886387284
yurikosano <yuriko.20p@gmail.com>
2022-04-11 14:20:48 +0000

    [add]注文一覧画面の追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,25 @@
 Rails.application.routes.draw do
-  namespace :admin do
-   root to: 'orders#index'
+  namespace :public do
+    get 'orders/new'
+    get 'orders/index'
+    get 'orders/show'
+  end
+  namespace :public do
+    root to: "homes#top"
+    get 'homes/about' => "homes#about"
+    get 'items/index'
+    get 'items/show'
+  end
+devise_for :admins, :controllers => {
+    registrations: "admins/registrations",
+    sessions: "admins/sessions",
+}
+namespace :admin do
+   root to: 'homes#top'
    resources :genres, only: [:index, :create, :edit, :update]
    resources :items, only: [:index, :new, :create, :show, :edit, :update]
    resources :customers, only: [:index, :show, :create, :edit, :update]
    resources :orders, only: [:index, :update]
   end
-  devise_for :admin
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_05_103229) do
+ActiveRecord::Schema.define(version: 2022_04_11_124740) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -95,6 +95,26 @@ ActiveRecord::Schema.define(version: 2022_04_05_103229) do
     t.integer "order_status"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "publics", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "last_name"
+    t.string "first_name"
+    t.string "last_name_kana"
+    t.string "first_name_kana"
+    t.string "postal_code"
+    t.string "address"
+    t.string "telephone_number"
+    t.boolean "is_active"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_publics_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_publics_on_reset_password_token", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
